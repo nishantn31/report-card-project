82c2478f48c4f982abbaacf277de1af40b3ea5bc
Nishant Nayan <nishantnayan@Nishants-MacBook-Air.local>
2023-12-03 16:35:49 +0530

    first commit

---
+++ core/reportcard/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import *
+from django.core.paginator import Paginator
+from django.db.models import Q, Sum
+# Create your views here.
+
+def get_students(request):
+    queryset = Student.objects.all()
+
+
+    if request.GET.get("search"):
+        search = request.GET.get("search")
+        queryset = queryset.filter(
+            Q(student_name__icontains = search)|
+            Q(department__department__icontains = search)|
+            Q(student_id__student_id__icontains = search)|
+            Q(student_email__icontains = search)|
+            Q(student_age__icontains = search)
+        )
+
+    paginator = Paginator(queryset, 10)  # Show 10 contacts per page.
+
+    page_number = request.GET.get("page", 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "report/students.html", {"queryset" : page_obj})
+
+
+def see_marks(request, student_id):
+    queryset = SubjectMarks.objects.filter(student__student_id__student_id = student_id)
+    total_marks = queryset.aggregate(total_marks = Sum("marks"))
+    
+
+
+
+    return render(request, "report/see_marks.html", {"queryset" : queryset, "total_marks" : total_marks})
